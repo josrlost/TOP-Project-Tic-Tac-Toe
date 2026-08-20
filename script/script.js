288acc354092734a8e85259d65b75c4ccabe62b1
josrlost <197001265+josrlost@users.noreply.github.com.>
2026-08-20 16:55:07 -0500

    Changed the privated variable that represents the board from within the gameboard object within the tictactoe IIFE. The board private variable no longer is an empty array, with two other private variables that represent the number of rows and columns, but now it is an matrix, an array that includes an array for the row, and an array for the number of columns. The gameboard object would also have a markingBoard method that represents the marking of the board move that includes a two dimensional for loop that loops based on the player input. Created a player composite object that has the method markingBoard method of the gameboard object

--- script/script.js
+++ script/script.js
@@ -1,13 +1,32 @@
 const ticTacToe = (() => {
     const gameboard = () => {
-        let board = [];
-        let column = 3;
-        let row = 3;
-        
+        let board = [
+            ["First Row", "Second Row", "Third Row"],
+            ["First Column", "Second Column", "Third Column"]
+        ];
+        let markingBoard = (playerInput = 0) => {
+            if(playerInput !== 0) {
+                return playerInput;
+            }
+            const [playerInputForRow, playerInputForColumn] = playerInput.split('');
+            +playerInputForColumn;
+            +playerInputForRow;
+            for(let i = 0; i < board[1].length; i + playerInputForColumn) {
+                return board[1][i];
+                for(let j = 0; j < board[0].length; j + playerInputForRow) {
+                    return board[0][j];
+                }
+            }
+        }
+        return {};
     }
-    function Player() {
-        let score;
-
+    function createPlayer(name) {
+        const { markingBoard } = gameboard;
+        return {
+            name,
+            markingBoard,
+        }
     }
 }
 )()
+
