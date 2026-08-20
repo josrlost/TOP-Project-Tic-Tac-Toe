847411c1dde9f9ba21ba46198d99970753ffc08f
josrlost <197001265+josrlost@users.noreply.github.com.>
2026-08-20 16:04:01 -0500

    Attempted to wrap in an IIFE the logic of the tic tac to game assigned into a single tictactoe global variable. The IIFE would wrap the gameboard object that is assigned to the returning value of an arrow function that has three private variables: one that represent the board as an empty array, a colum and a row variable that store the maximum number of columns and rows for a game of tic tact toe

---
+++ script/script.js
@@ -0,0 +1,13 @@
+const ticTacToe = (() => {
+    const gameboard = () => {
+        let board = [];
+        let column = 3;
+        let row = 3;
+        
+    }
+    function Player() {
+        let score;
+
+    }
+}
+)()
